0e842719a4869e3dabbe8ce0967344d743dbbb95
liuyongjin <YongJin_Liu@jabil.com>
2023-06-14 16:18:10 +0800

    feat: index页面添加粒子特效

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,101 @@
 import { Component } from '@angular/core';
+import { loadFull } from 'tsparticles';
+import {
+  ClickMode,
+  // Container,
+  Engine,
+  HoverMode,
+  MoveDirection,
+  OutMode,
+} from 'tsparticles-engine';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent {
+  id = 'tsparticles';
 
+  /* or the classic JavaScript object */
+  particlesOptions = {
+    // background: {
+    //   color: {
+    //     // value: '#0d47a1',
+    //   },
+    // },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: ClickMode.push,
+        },
+        onHover: {
+          enable: true,
+          mode: HoverMode.repulse,
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: '#ffffff',
+      },
+      links: {
+        color: '#ffffff',
+        distance: 150,
+        enable: true,
+        opacity: 0.3,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.3,
+      },
+      shape: {
+        type: 'circle',
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  // particlesLoaded(container: Container): void {
+  //   console.log(container);
+  // }
+
+  async particlesInit(engine: Engine): Promise<void> {
+    await loadFull(engine);
+  }
 }
--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
+import { NgParticlesModule } from 'ng-particles';
 
 import { IndexComponent } from './index.component';
 import { IndexRoutingModule } from './index-routing.module';
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [CommonModule, SharedModule, IndexRoutingModule],
+  imports: [CommonModule, SharedModule, IndexRoutingModule, NgParticlesModule],
 })
 export class IndexModule {}
